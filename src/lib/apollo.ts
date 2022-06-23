820a241de155a19b4fb347748aeb08a91ef319f7
Jonatas Freitas <jonatasfreitas14@hotmail.com>
2022-06-23 18:59:32 -0300

    Projeto ReactJS

---
+++ src/lib/apollo.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://api-us-west-2.graphcms.com/v2/cl4okeftf0iqo01w7avrndr95/master',
+    cache: new InMemoryCache()
+
+})
